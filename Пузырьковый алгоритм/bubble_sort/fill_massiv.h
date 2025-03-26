26d3aca0dd2dc070364a1269403e5605b5e591b7
k0swel <romanleonenko32@gmail.com>
2025-03-26 19:29:29 +0300

    Пузырьковый алгоритм

---
+++ Пузырьковый алгоритм/bubble_sort/fill_massiv.h
@@ -0,0 +1,11 @@
+#ifndef fill_massiv
+#define fill_massov
+
+#include <chrono>
+#include <random>
+#include <vector>
+
+std::vector<int> create_rand_massiv(int size, int min_rand_number, int max_rand_number);
+
+
+#endif
